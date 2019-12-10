7b8c482b773c7fc92bff262ed3ed727ddb8d6ac7
qizhiwen <1970874980@qq.com>
2019-12-10 15:30:50 +0800

    初始化

---
+++ dubbo-cosumer/src/main/java/com/sy/DubboCosumerApplication.java
@@ -0,0 +1,13 @@
+package com.sy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboCosumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboCosumerApplication.class, args);
+    }
+
+}
